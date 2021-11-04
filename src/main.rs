ef63b36ab9e50741dc0bd4e9002dbcd80357c65d
Cristian Eigel <cristian.eigel@esrlabs.com>
2021-11-04 18:22:57 +0100

    Log the ticks in an array

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,39 @@ use rtic::app;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32f4xx_hal::gpio::{gpioa, gpioc, Alternate, Output, PushPull, AF2};
 
+fn increment_vals(slice: &mut [u8]) {
+    for a in slice {
+        *a += 1;
+    }
+}
+
+fn add_tick(arr: &mut [u8], span: usize, idx: usize) {
+    let from = if idx < span { 0 } else { idx - span };
+    let to = if idx + span > arr.len() {
+        arr.len()
+    } else {
+        idx + span
+    };
+    increment_vals(&mut arr[from..to]);
+    if idx < span {
+        let from = arr.len() - span + idx;
+        increment_vals(&mut arr[from..]);
+    } else if arr.len() <= idx + span {
+        let to = idx + span - arr.len();
+        increment_vals(&mut arr[0..to]);
+    }
+}
+
 #[app(device = feather_f405::hal::stm32, monotonic = rtic::cyccnt::CYCCNT, peripherals = true)]
 const APP: () = {
     struct Resources {
         dcf_pin: gpioa::PA6<Alternate<2>>,
         debug_pin: gpioc::PC6<Output<PushPull>>,
         timer: pac::TIM3,
+        #[init([0;1000])]
+        triggers: [u8; 1000],
+        #[init(0)]
+        count: u8,
     }
     #[init(spawn=[])]
     fn init(cx: init::Context) -> init::LateResources {
@@ -42,7 +69,7 @@ const APP: () = {
         let psc = ((2 * clocks.pclk1().0) / 10000 as u32) as u16 - 1;
 
         timer.psc.write(|w| w.psc().bits(psc));
-        timer.arr.write(|w| w.arr().bits(u16::MAX));
+        timer.arr.write(|w| w.arr().bits(10_000));
 
         timer.cr1.modify(|_, w| w.urs().set_bit());
         timer.egr.write(|w| w.ug().set_bit());
@@ -64,12 +91,12 @@ const APP: () = {
             w.cc2e().set_bit();
             w
         });
-        timer.smcr.write(|w| {
+        /*timer.smcr.write(|w| {
             w.etp().inverted();
             w.sms().reset_mode();
             w.ts().ti1fp1();
             w
-        });
+        });*/
 
         timer.dier.write(|w| w.cc1ie().set_bit().cc2ie().set_bit());
         timer.cr1.modify(|_, w| {
@@ -91,24 +118,20 @@ const APP: () = {
         loop {}
     }
 
-    #[task(binds = TIM3, priority=2, resources=[timer, dcf_pin, debug_pin])]
+    #[task(binds = TIM3, priority=2, resources=[timer, dcf_pin, debug_pin, triggers, count])]
     fn tim3(cx: tim3::Context) {
         let timer: &mut pac::TIM3 = cx.resources.timer;
         let flags = timer.sr.read();
-        let fbits: u32 = flags.bits();
+        let count: &mut u8 = cx.resources.count;
         if flags.cc1if().is_match_() {
             let c1 = timer.ccr1.read().bits();
-            rprintln!("c1 {}", c1);
+            add_tick(cx.resources.triggers, 10, (c1 / 10) as usize);
+            *count += 1;
+            if *count == 60 {
+                *count = 0;
+                rprintln!(=>1, "{:?}", cx.resources.triggers);
+            }
         }
-        if flags.cc2if().is_match_() {
-            let c2 = timer.ccr2.read().bits();
-            rprintln!("c2 {}", c2);
-        }
-        if !flags.cc1if().is_match_() && !flags.cc2if().is_match_() {
-            rprintln!("NONE");
-        }
-        let cnt = timer.cnt.read().bits();
-        rprintln!("cnt: {}, flags: {:016b}", cnt, fbits);
         timer.sr.modify(|_, w| {
             w.uif().clear_bit();
             w.tif().clear_bit();
